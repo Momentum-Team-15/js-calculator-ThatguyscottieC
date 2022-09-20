547b8d735fa026880964ab1d34764508115a13d1
ThatguyscottieC <officialscottiechambers@gmail.com>
2022-09-20 15:51:40 -0400

    finished calc

---
+++ main.js
@@ -0,0 +1,20 @@
+console.log("hello world");
+
+let display = document.getElementById("display");
+
+let buttons = Array.from(document.getElementsByClassName("button"));
+
+buttons.map((button) => {
+  button.addEventListener("click", (e) => {
+    switch (e.target.innerText) {
+      case "C":
+        display.innerText = "";
+        break;
+      case "=":
+        display.innerText = eval(display.innerText);
+        break;
+      default:
+        display.innerText += e.target.innerText;
+    }
+  });
+});
